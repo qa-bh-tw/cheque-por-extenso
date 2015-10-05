dcb5ef6473a36544f2ef3e516f1c83cb8e95ae32
Lucas Tadeu Farias de Ávila-LAVILA4 <lavila4@vmcommerce.intra>
2015-10-05 20:51:52 -0300

    refatora metodo para extrair unidades e dezenas

--- src/main/java/ChequePorExtenso.java
+++ src/main/java/ChequePorExtenso.java
@@ -4,33 +4,28 @@ public class ChequePorExtenso {
     public static final String REAL = " Real";
     public static final String CEM = "Cem";
 
+
+    private final String menoresQueVinte[] = {"Zero", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito",
+            "Nove", "Dez", "Onze", "Doze", "Treze", "Quatorze", "Quinze", "Dezesseis", "Dezessete",
+            "Dezoito", "Dezenove"};
+
+    private final String dezenas[] = {"", "", "Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta",
+            "Noventa"};
+
+    private final String centenas[] = {"", "Cento", "Duzentos", "Trezentos", "Quatrocentos", "Quinhentos", "Seiscentos",
+            "Setecentos", "Oitocentos", "Novecentos"};
+
+
     public String getValorPorExtenso(final int valor) {
 
+        String valorPorExtenso = "";
         int indice;
         int indiceUnidades = valor % 10;
         int indiceDezenas = valor / 10;
         int indiceCentenas = valor / 100;
 
-        String menoresQueVinte[] = {"Zero", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito",
-                "Nove", "Dez", "Onze", "Doze", "Treze", "Quatorze", "Quinze", "Dezesseis", "Dezessete",
-                "Dezoito", "Dezenove"};
-
-        String dezenas[] = {"", "", "Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta",
-                "Noventa"};
-
-        String centenas[] = {"", "Cento", "Duzentos", "Trezentos", "Quatrocentos", "Quinhentos", "Seiscentos",
-                "Setecentos", "Oitocentos", "Novecentos"};
-
-        String valorPorExtenso = "";
-
         if (valor < 100) {
-            if (valor >= 0 && valor < 20) {
-                valorPorExtenso = menoresQueVinte[valor];
-            } else if (ehDezenaRedonda(valor) && valor < 100) {
-                valorPorExtenso = dezenas[valor / 10];
-            } else if (valor > 20 && valor < 100) {
-                valorPorExtenso = dezenas[indiceDezenas] + " e " + menoresQueVinte[indiceUnidades];
-            }
+            valorPorExtenso = retornaValorPorExtensoUnidadesEDezenas(valor);
         } else if (valor >= 100) {
             if (valor == 100) {
                 valorPorExtenso = CEM;
@@ -46,6 +41,20 @@ public class ChequePorExtenso {
 
         return valorPorExtenso;
 
+    }
+
+    private String retornaValorPorExtensoUnidadesEDezenas(int valor) {
+        int indiceUnidades = valor % 10;
+        int indiceDezenas = valor / 10;
+        String valorPorExtenso = "";
+        if (valor >= 0 && valor < 20) {
+            valorPorExtenso = menoresQueVinte[valor];
+        } else if (ehDezenaRedonda(valor)) {
+            valorPorExtenso = dezenas[valor / 10];
+        } else if (valor > 20) {
+            valorPorExtenso = dezenas[indiceDezenas] + " e " + menoresQueVinte[indiceUnidades];
+        }
+        return valorPorExtenso;
     }
 
     private boolean ehDezenaRedonda(int valor) {
